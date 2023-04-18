d0de7f34db92f6b19565b9097a4e034dd1139c07
agil20 <100156146+agil20@users.noreply.github.com>
2023-04-18 00:17:14 -0700

    feat:get mmc Action elave etdim

--- WebApplication/WebApplication/Areas/AdminPanel/Controllers/DashboardController.cs
+++ WebApplication/WebApplication/Areas/AdminPanel/Controllers/DashboardController.cs
@@ -1,16 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace WebApplication.Areas.AdminPanel.Controllers
 {
+    [Area("AdminPanel")]
     public class DashboardController : Controller
     {
-        [Area("AdminPanel")]
+       
 
         public IActionResult Index()
         {
             
             return View();
         }
+        public async Task<IActionResult> GetMMS()
+        {
+
+            return View();
+        }
+
     }
 }
